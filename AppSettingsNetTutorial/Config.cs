9c985c73cfeb097d33a3e1bbe0085248b117ea25
CodeArco <codearconet@gmail.com>
2025-08-04 06:44:27 +0200

    Done

---
+++ AppSettingsNetTutorial/Config.cs
@@ -0,0 +1,35 @@
+﻿namespace AppSettingsNetTutorial
+{
+    public class Config
+    {
+        public string ExampleString { get; set; }
+        public int ExampleInt { get; set; }
+        public ExampleObject ExampleObject { get; set; }
+        public List<string> ExampleStringList { get; set; }
+        public List<ExampleObjectListItem> ExampleObjectList { get; set; }
+    }
+
+    public class ExampleObject
+    {
+        public string Property1 { get; set; }
+        public int Property2 { get; set; }
+    }
+
+    public class ExampleObjectListItem
+    {
+        public Header Header { get; set; }
+        public Body Body { get; set; }
+    }
+
+    public class Header
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class Body
+    {
+        public string Content { get; set; }
+        public double Details { get; set; }
+    }
+}
---
+++ AppSettingsNetTutorial/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+using AppSettingsNetTutorial;
+using Microsoft.Extensions.Configuration;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var build = new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json")
+        .Build();
+
+        // Alternative 1
+        var config1 = build.Get<Config>();
+
+        // Alternative 2
+        var config2 = new Config();
+        build.Bind(config2);
+
+        // Output
+        Console.WriteLine($"config1: {config1?.ExampleObjectList.FirstOrDefault()?.Header.Description}");
+        Console.WriteLine($"config2: {config2.ExampleObjectList.FirstOrDefault()?.Header.Description}");
+    }
+}
